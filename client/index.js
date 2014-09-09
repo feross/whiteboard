b852a042a4e4f776e4cbcd7b91a383b26aa4c4d1
Feross Aboukhadijeh <feross@feross.org>
2014-09-09 09:50:18 +0100

    webtorrent working

--- client/index.js
+++ client/index.js
@@ -1,12 +1,29 @@
+var Client = require('bittorrent-client')
+var concat = require('concat-stream')
+var dragDrop = require('drag-drop/buffer')
 var hat = require('hat')
 var Tracker = require('webtorrent-tracker')
 
-var canvas, ctx
+var username = window.prompt('What is your name?')
+var color = 'rgb(' + hat(8, 10) + ',' + hat(8, 10) + ',' + hat(8, 10) + ')'
+var currentPathId = null
+var state = {}
+var imgs = {}
 
-function createCanvas () {
-  canvas = window.canvas = document.createElement('canvas')
-  ctx = window.ctx = canvas.getContext('2d')
+var peers = []
+var peerId = new Buffer(hat(160), 'hex')
+var client = new Client({ peerId: peerId })
 
+// create canvas
+var canvas = document.createElement('canvas')
+var ctx = canvas.getContext('2d')
+document.body.appendChild(canvas)
+
+// set canvas settings and size
+setupCanvas()
+window.addEventListener('resize', setupCanvas)
+
+function setupCanvas () {
   // calculate scale factor for retina displays
   var devicePixelRatio = window.devicePixelRatio || 1
   var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
@@ -26,66 +43,84 @@ function createCanvas () {
   // set stroke options
   ctx.lineCap = 'round'
   ctx.lineJoin = 'round'
-  ctx.strokeStyle = 'rgb(0,0,0)'
   ctx.lineWidth = 5
 
   // set font options
   ctx.fillStyle = 'rgb(255,0,0)'
-  ctx.font='16px sans-serif'
-
-  document.body.appendChild(canvas)
+  ctx.font ='16px sans-serif'
+  redraw()
 }
-
-var paths = {}
-var currentPathId = null
-var peers = []
 
 function redraw () {
   // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  // loop all paths
-  Object.keys(paths).forEach(function (pathId) {
+  // draw the current state
+  Object.keys(state).forEach(function (id) {
+    var data = state[id]
 
-    // draw each path
-    var path = paths[pathId]
-    ctx.beginPath()
-    path.forEach(function (point, i) {
-      if (i === 0) ctx.moveTo(point.x, point.y)
-      else ctx.lineTo(point.x, point.y)
-    })
-    ctx.stroke()
+    // draw paths
+    if (data.pts) {
+      ctx.beginPath()
+      ctx.strokeStyle = data.color
+      data.pts.forEach(function (point, i) {
+        if (i === 0) ctx.moveTo(point.x, point.y)
+        else ctx.lineTo(point.x, point.y)
+      })
+      ctx.stroke()
+    }
+
+    // draw images
+    if (data.infoHash) {
+      if (!imgs[data.infoHash]) {
+        imgs[data.infoHash] = { complete: false, img: null }
+        client.download({
+          infoHash: data.infoHash,
+          announce: [ 'ws://tracker.webtorrent.io:9003' ]
+        }, function (torrent) {
+          torrent.files[0].createReadStream().pipe(concat(function (buf) {
+            bufToImage(buf, function (img) {
+              imgs[data.infoHash] = { complete: true, img: img }
+              redraw()
+            })
+          }))
+        })
+
+        ctx.fillRect(data.pos.x - 100, data.pos.y - 100, 200, 200)
+        return
+      }
+      if (imgs[data.infoHash].complete) {
+        var img = imgs[data.infoHash].img
+        ctx.drawImage(img, data.pos.x - 100, data.pos.y - 100, 200, 200)
+      }
+    }
   })
 
   // draw usernames
-  peers.forEach(function (peer, i) {
-    var username = peer.username || '<new peer>'
-    ctx.fillText(username, 20, window.innerHeight - 20 - (i * 20))
-  })
-}
-
-var id = new Buffer(hat(160), 'hex')
-var username = window.prompt('What is your name?')
-
-function startTracker () {
-  var tracker = new Tracker(id, {
-    announce: [ 'wss://tracker.webtorrent.io' ],
-    infoHash: new Buffer(20)
-  })
-
-  tracker.start()
-
-  tracker.on('peer', function (peer) {
-    window.peer = peer
-    peers.push(peer)
-    peer.send({ id: id.toString('hex'), username: username })
-    peer.send({ paths: paths })
-    peer.on('message', onMessage.bind(undefined, peer))
-    peer.on('close', function () {
-      peers.splice(peers.indexOf(peer), 1)
+  peers.concat({ color: color, username: username })
+    .forEach(function (peer, i) {
+      var username = peer.username || '<new peer>'
+      ctx.fillStyle = peer.color
+      ctx.fillText(username, 20, window.innerHeight - 20 - (i * 20))
     })
-  })
 }
+
+var tracker = new Tracker(peerId, {
+  announce: [ 'ws://tracker.webtorrent.io:9003' ],
+  infoHash: new Buffer(20)
+})
+
+tracker.start()
+
+tracker.on('peer', function (peer) {
+  peers.push(peer)
+  peer.send({ username: username, color: color, state: state })
+  peer.on('message', onMessage.bind(undefined, peer))
+  peer.on('close', function () {
+    peers.splice(peers.indexOf(peer), 1)
+    redraw()
+  })
+})
 
 function broadcast (obj) {
   peers.forEach(function (peer) {
@@ -94,46 +129,44 @@ function broadcast (obj) {
 }
 
 function onMessage (peer, data) {
-  console.log(data)
-
-  if (data.username && data.id) {
+  if (data.username) {
     peer.username = data.username
-    peer.id = data.id
+    peer.color = data.color
     redraw()
   }
 
-  if (data.paths) {
-    console.log('got paths' + data.paths)
-    Object.keys(data.paths)
-      .filter(function (pathId) {
-        return !paths[pathId]
+  if (data.state) {
+    Object.keys(data.state)
+      .filter(function (id) {
+        return !state[id]
       })
-      .forEach(function (pathId) {
-        paths[pathId] = data.paths[pathId]
+      .forEach(function (id) {
+        state[id] = data.state[id]
       })
     redraw()
   }
 
-  if (data.i && data.p) {
-    if (!paths[data.i]) paths[data.i] = []
-    paths[data.i].push(data.p)
+  if (data.pt) {
+    if (!state[data.i]) state[data.i] = { pts: [], color: data.color }
+    state[data.i].pts.push(data.pt)
+    redraw()
+  }
+
+  if (data.infoHash) {
+    state[data.infoHash] = data
     redraw()
   }
 }
-
-startTracker()
-createCanvas()
 
 canvas.addEventListener('mousedown', function (e) {
   currentPathId = hat(80)
   var p1 = { x: e.clientX, y: e.clientY }
   var p2 = { x: e.clientX + 0.001, y: e.clientY + 0.001 } // paint point on click
 
-  paths[currentPathId] = [ p1, p2 ]
+  state[currentPathId] = { color: color, pts: [ p1, p2 ] }
+  broadcast({ i: currentPathId, pt: p1, color: color })
+  broadcast({ i: currentPathId, pt: p2 })
   redraw()
-
-  broadcast({ i: currentPathId, p: p1 })
-  broadcast({ i: currentPathId, p: p2 })
 })
 
 document.body.addEventListener('mouseup', function () {
@@ -142,9 +175,35 @@ document.body.addEventListener('mouseup', function () {
 
 canvas.addEventListener('mousemove', function (e) {
   if (currentPathId) {
-    var p = { x: e.clientX, y: e.clientY }
-    paths[currentPathId].push(p)
-    broadcast({ i: currentPathId, p: p })
+    var pt = { x: e.clientX, y: e.clientY }
+    state[currentPathId].pts.push(pt)
+    broadcast({ i: currentPathId, pt: pt })
     redraw()
   }
 })
+
+dragDrop('body', function (files, pos) {
+  client.seed(files, function (torrent) {
+    var message = {
+      infoHash: torrent.infoHash,
+      pos: pos
+    }
+    broadcast(message)
+    state[torrent.infoHash] = message
+
+    bufToImage(files[0].buffer, function (img) {
+      imgs[torrent.infoHash] = { complete: true, img: img }
+      redraw()
+    })
+  })
+})
+
+function bufToImage (buf, cb) {
+  var img = new Image()
+  img.src = URL.createObjectURL(
+    new Blob([ buf ])
+  )
+  img.onload = function () {
+    cb(img)
+  }
+}
