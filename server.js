89e289147b6c3097279b328c633644867f2b4eb3
Feross Aboukhadijeh <feross@feross.org>
2017-01-24 23:35:33 -0800

    standard

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var compression = require('compression')
 var express = require('express')
+var path = require('path')
 
 var port = process.argv[2] || 3000
 var app = express()
 app.use(compression())
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(path.join(__dirname, 'static')))
 app.listen(port, function () {
   console.log('listening on port ' + port)
 })
